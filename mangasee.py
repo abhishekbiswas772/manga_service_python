651aa89c5089ad714a52a99645d6dd0d5e85a5b4
Abhishek Biswas <128672478+abhishekbiswas772@users.noreply.github.com>
2024-06-17 15:17:07 +0530

    fix

--- mangasee.py
+++ mangasee.py
@@ -15,8 +15,7 @@ class MangaSeeAPI:
     def search_manga(self, query):
         try:
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Referer': 'https://mangasee123.com'
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
             }
             response = requests.get('https://mangasee123.com/_search.php', headers = headers)
             response.raise_for_status()
